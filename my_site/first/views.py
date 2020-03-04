d4b2e24a0d4500f9684d7427693ab99ec51dce88
Poornartha Sawant <spoornartha@gmail.com>
2020-03-05 02:55:03 +0530

    Updated

--- my_site/first/views.py
+++ my_site/first/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 from . import models
 from django.utils import timezone
 from bs4 import BeautifulSoup
+from requests.compat import quote_plus
 
-BASE_FLIPKART_URL = 'https://www.flipkart.com/search?q={}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
-BASE_IMAGE_URL = 'https://rukminim1.flixcart.com/image/312/312/k0lbdzk0pkrrdj/mobile-refurbished/z/j/2/c2-16-u-rmx1945-realme-2-original-imaffnumygt8wgfx.jpeg?q=70'
+BASE_FLIPKART_URL = 'https://www.bewakoof.com/search/{}'
 
 # Create your views here.
 '''search?q=xiomi&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off
 search?q=realme&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off
 /search?q=realme%20phone&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'''
+
 
 def home(request):
     return render(request, 'base.html')
@@ -40,29 +41,45 @@ def save_prod(request):
 
 def scrape(request):
     search = request.POST.get('search')
-    final_url = BASE_FLIPKART_URL.format(search)
+    final_url = BASE_FLIPKART_URL.format(quote_plus(search))
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
-    post_listings = soup.find_all('div', {'class': '_1UoZlX'})
+    wrap = soup.find('div', {'class': 'categoryGridWrapper'})
+    image_set = wrap.find_all('img')
+    link_set = wrap.find_all('a')
+    image_collection = []
+    link_collection = []
+
+    for link in link_set:
+        check = link.get('href')
+        link_collection.append(check)
+
+
+    for image in image_set:
+        check = image['src']
+        if check != "https://images.bewakoof.com/web/b-coin-gold-3x.png":
+            image_collection.append(check)
+
+    for image in image_collection:
+        if image == "https://images.bewakoof.com/web/b-coin-gold-3x.png":
+            image_collection.remove(image)
+
+    post_listings = soup.find_all('div', {'class': 'productCardDetail'})
     final_postings = []
-    for post in post_listings:
-        post_title = post.find(class_='_3wU53n').text
-        post_url = post.find('a').get('href')
-
-        if post.find(class_='result-price'):
-            post_price = post.find(class_='_1vC4OE _2rQ-NK').text
-        else:
-            post_price = 'N/A'
-
-        post_image_url = BASE_IMAGE_URL
-
-        final_postings.append((post_title, post_url, post_price, post_image_url))
+    i = 0
+    while i<10:
+        post = post_listings[i]
+        post_title = post.find('h3').text
+        post_price = post.find('b').text
+        link_base = "https://www.bewakoof.com{}"
+        final_postings.append((post_title, post_price, image_collection[i], link_base.format(link_collection[i+5])))
+        i = i + 1
 
     stuff_for_frontend = {
         'search': search,
         'listings': final_postings,
     }
 
-    return render(request, 'first/sc_post.html', stuff_for_frontend)
+    return render(request, 'first/new_search.html', stuff_for_frontend)
 
