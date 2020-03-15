d0e0c530a2d9d1be5d5b096516cd6364198d49c1
unknown <spoornartha@gmail.com>
2020-03-15 09:25:55 +0530

    Resetting Urls

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('https://poornartha.github.io/webscraper/', include('first.urls')),
+    path('', include('first.urls')),
     path('admin/', admin.site.urls),
 ]
 
