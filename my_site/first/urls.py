3a24bb2bdb1f40d52ea209e80717edc11c83cb0e
unknown <spoornartha@gmail.com>
2020-03-15 09:14:01 +0530

    Hosting Config

--- my_site/first/urls.py
+++ my_site/first/urls.py
@@ -3,10 +3,10 @@ from . import views, admin
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('new_search', views.new_search, name="new_search"),
-    path('new_prod', views.new_prod, name="new_prod"),
-    path('save_product', views.save_prod, name="save_prod"),
-    path('new_scrape', views.scrape, name="new_scrape"),
+    path('https://poornartha.github.io/webscraper/', views.home, name='home'),
+    path('https://poornartha.github.io/webscraper/new_search', views.new_search, name="new_search"),
+    path('https://poornartha.github.io/webscraper/new_prod', views.new_prod, name="new_prod"),
+    path('https://poornartha.github.io/webscraper/save_product', views.save_prod, name="save_prod"),
+    path('https://poornartha.github.io/webscraper/new_scrape', views.scrape, name="new_scrape"),
 ]
 
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', include('first.urls')),
+    path('https://poornartha.github.io/webscraper/', include('first.urls')),
     path('admin/', admin.site.urls),
 ]
 
